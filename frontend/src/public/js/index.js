5aaee658a6ad46b65c8351df4f8098e2b99126b3
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-18 21:14:46 +0200

    Add functions to update xp bar and level status

--- frontend/src/public/js/index.js
+++ frontend/src/public/js/index.js
@@ -1,6 +1,20 @@
-function setXpBar() {
-    xpBar = document.getElementById("header-xp-progress-bar-front");
+function updateXpBar() {
+    const xpBar = document.getElementById("header-xp-progress-bar-front");
     xpBar.style.width = getXpPercent() + "%";
 }
 
-window.onload = setXpBar;
+function updateLevel() {
+    const levelNumber = document.getElementById("header-level-number");
+    levelNumber.innerHTML = getLevel();
+}
+
+function updateLevelTitle() {
+    const levelTitle = document.getElementById("header-level-title");
+    levelTitle.innerHTML = getLevelTitle();
+}
+
+window.onload = function() {
+    updateXpBar();
+    updateLevel();
+    updateLevelTitle();
+}
