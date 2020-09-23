696a8130d845c0b2c5e18e0df2eb02d1d4397d51
osksun <37369260+osksun@users.noreply.github.com>
2020-09-23 17:00:52 +0200

    Fixed bug

---
+++ frontend/src/public/js/2d-visualization.js
@@ -0,0 +1,21 @@
+//import Chart from 'chart.js'; 
+function updateChart(chart, value){
+    chart.data.datasets[0].data[0] = slider0.value
+    chart.update();
+}
+
+function addData(chart, label, data) {
+    chart.data.labels.push(label);
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    });
+    chart.update();
+}
+
+function removeData(chart) {
+    chart.data.labels.pop();
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.pop();
+    });
+    chart.update();
+}
