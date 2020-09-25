1abcb5dfbde27774e3d0ab5d35cac80096af2380
MarqusJonsson <markusjonsson1997@gmail.com>
2020-09-25 18:39:50 +0200

    Add script to handle category buttons and views

--- frontend/src/public/js/index.js
+++ frontend/src/public/js/index.js
@@ -10,10 +10,33 @@ function updateLevelInfo() {
     levelNumber.innerHTML = "Level " + getLevel() + " | " + getLevelTitle();
 }
 
+function setupCategoryButtons(buttonContainerID, viewContainerID) {
+    const topCategoryButtons = document.getElementById(buttonContainerID).getElementsByTagName("button");
+    const topCategoryViews = document.getElementById(viewContainerID).getElementsByTagName("ul");
+    for (let i = 0; i < topCategoryButtons.length; i++) {
+        let btns = [...topCategoryButtons];
+        let views = [...topCategoryViews];
+        btns.splice(i, 1);
+        views.splice(i, 1);
+        topCategoryButtons[i].addEventListener("click", function() {
+            toggleView(topCategoryButtons[i], topCategoryViews[i], btns, views)
+        });
+    }
+}
+
+function toggleView(activeButton, visibleView, inactiveButtons, hiddenViews) {
+    activeButton.classList.add("active");
+    visibleView.classList.add("visible");
+    inactiveButtons.forEach(button => button.classList.remove("active"));
+    hiddenViews.forEach(view => view.classList.remove("visible"));
+}
+
 window.onload = function () {
     updateXpBar(getXpPercent());
     updateLevelInfo();
     setInterval(() => {
        updateXpBar(Math.random() * 100);
     }, 1000);
+    setupCategoryButtons("top-button-container", "top-view-container");
+    setupCategoryButtons("bot-button-container", "bot-view-container");
 }
