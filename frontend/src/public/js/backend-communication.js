5d1cf95fcf4380c9637f2c8d070cd51998019c6b
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-06 16:01:42 +0200

    Fix ajaxPost with content

--- frontend/src/public/js/backend-communication.js
+++ frontend/src/public/js/backend-communication.js
@@ -2,18 +2,23 @@ function getCourses() {
     return ajaxPost("http://127.0.0.1:80/getcourses");
 }
 
-function getQuestions(course, module) {
-    return ajaxPost("http://127.0.0.1:80/getquestions", encodeURI("course=" + course + "&module=" + module));
+function getModules(courseName) {
+    return ajaxPost("http://127.0.0.1:80/getmodules", encodeURI("course=" + courseName));
 }
 
-function getModules(course) {
-    return ajaxPost("http://127.0.0.1:80/getmodules", encodeURI("course=" + course));
+function getQuestions(courseName, moduleName) {
+    return ajaxPost("http://127.0.0.1:80/getquestions", encodeURI("course=" + courseName + "&module=" + moduleName));
+}
+
+function getQuestion(id) {
+    return ajaxPost("http://127.0.0.1:80/getquestion", encodeURI("question=" + id));
 }
 
 function ajaxPost(url, data = "") {
     return new Promise((resolve, reject) => {
         const request = new XMLHttpRequest();
         request.open("POST", url, true);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
         request.onreadystatechange = () => {
             if(request.readyState === XMLHttpRequest.DONE) {
                 const status = request.status;
