a7678118b4a331ca457fd54986a48f3703e34115
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-07 18:21:47 +0200

    Update to use namespace DbCom

--- frontend/src/question/js/question.js
+++ frontend/src/question/js/question.js
@@ -1,12 +1,12 @@
 
 window.addEventListener("load",() => {
-    const questionID = 1;
+    const questionID = 2;
     const submitButton = document.getElementById("button-submit");
     submitButton.addEventListener("click", handleSubmit);
     const questionElement = document.getElementById("question")
     const coruseNameElement = document.getElementById("course-name")
     const moduleNameElement = document.getElementById("module-name")
-    getQuestion(questionID).then((question) => {
+    DbCom.getQuestion(questionID).then((question) => {
         questionElement.innerText = question.content;
         coruseNameElement.innerText = question.course;
         moduleNameElement.innerText = question.module;
@@ -18,7 +18,7 @@ window.addEventListener("load",() => {
         const loadingIcon = document.getElementById("loading-icon");
         submitButton.style.display = "none";
         loadingIcon.style.display = "inline-block";
-        answerQuestion(questionID, answerInput.value).then((result) => {
+        DbCom.answerQuestion(questionID, answerInput.value).then((result) => {
             if (result.correct) {
                 answerInput.style.backgroundColor = "#00ff00";
             } else {
@@ -26,7 +26,6 @@ window.addEventListener("load",() => {
             }
             loadingIcon.style.display = "none";
             submitButton.style.display = "inline-block";
-            console.log(result);
         });
     }
 });
