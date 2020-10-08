d3ed40c8e3d5fbf5e7398fb71927fa029bbee6bd
Jonathan <>
2020-10-08 16:03:19 +0200

    NOT NULL

--- backend/auth/database.js
+++ backend/auth/database.js
@@ -15,7 +15,7 @@ function connect() {
 			} else {
 				console.log("MySQL database connected!");
 
-				connection.query("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) UNIQUE, password VARCHAR(255), PRIMARY KEY(id))", (error, result) => {
+				connection.query("CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT, email VARCHAR(255) UNIQUE NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))", (error, result) => {
 					if(error) {
 						console.error("Failed to create database tables!");
 						reject();
