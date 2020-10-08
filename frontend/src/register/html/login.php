c77351b3fa59a05e2fce54fd62b7e13d79669740
MarqusJonsson <markusjonsson1997@gmail.com>
2020-10-08 20:26:49 +0200

    Create login.php
    Co-Authored-By: Tim Coull <25230258+Ticou93@users.noreply.github.com>

---
+++ frontend/src/register/html/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>D7057E</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../css/register.css">
+        <link rel="stylesheet" type="text/css" href="../../shared/css/body.css">
+        <script type="application/javascript" src="../../shared/js/backend-communication.js"></script>
+    </head>
+    <body>
+        <!--LOGIN FORM SECTION -->
+        <div id="registerContainer">
+            <div id="titleContainer">
+                <h2>LOGIN</h2>
+            </div>
+            <div id="contentContainer">
+                <label class="uniLabel" for="uname">Username:</label>
+                <input type="text" class="inputContent" placeholder="First Name" name="fname">
+                <br><br>
+                <label class="uniLabel" for="sname">Password:</label>
+                <input type="password" class="inputContent" placeholder="password" name="sname">
+                <br><br>
+                <input type="submit" name="Register" value="Register">
+            </div>
+        </div>
+    </body>
+</html>
+
