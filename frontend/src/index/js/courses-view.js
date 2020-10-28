1f08a243e4a52b2bbd52fa0057da9d682a1a12f7
Jonathan <>
2020-10-28 13:33:03 +0100

    Add course card

--- frontend/src/index/js/courses-view.js
+++ frontend/src/index/js/courses-view.js
@@ -23,10 +23,35 @@ const coursesView = new function() {
         return card;
     }
 
+		function createAdminCreateCard() {
+        const card = document.createElement("li");
+        card.className = "course-card";
+        const a = document.createElement("a");
+				a.href = "/createcourse";
+        const header = document.createElement("div");
+        header.className = "course-card-header";
+        header.style.backgroundColor = "#fff";
+        const title = document.createElement("h3");
+        header.appendChild(title);
+        const info = document.createElement("ul");
+        info.innerHTML = "<img style=\"display:block;margin:20px auto 35px auto;width:64px;\" src=\"/src/index/svg/add.svg\">";
+        a.appendChild(header);
+        a.appendChild(info);
+        card.appendChild(a);
+        return card;
+		}
+
     this.createCards = function(courses) {
         courses.forEach((course) => {
             coursesViewDiv.appendChild(createCard(course.id, course.name, "#" + course.color));
         });
+
+				DbCom.isAdmin().then((result) => {
+					if(result.isAdmin) {
+						coursesViewDiv.appendChild(createAdminCreateCard());
+					}
+				});
+
     };
 
     this.clear = function() {
