7cfb87dc7c9854a46e4101f251bfdddfd4ebcced
Jonathan <>
2020-10-28 10:42:31 +0100

    Course editor

---
+++ frontend/src/course-editor/html/course-editor.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Create course</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" href="../../shared/css/body.css">
+        <link rel="stylesheet" type="text/css" href="../css/course-editor.css">
+        <link rel="stylesheet" type="text/css" href="../../shared/css/fixed-content.css">
+        <script type="application/javascript" src="../../shared/js/backend-communication.js"></script>
+        <script type="application/javascript" src="../../shared/js/fixed-content.js" defer></script>
+        <script type="application/javascript" src="../js/course-editor.js" defer></script>
+    </head>
+    <body>
+        <?php include "../../shared/html/fixed-content.html"; ?>
+        <div id="page-content">
+            <div><h3>Course name</h3><input id="courseName" type="text" minlength=1 maxlength=255 required/><input id="courseColor" type="color"/></div>
+						<div><h3>Description</h3><textarea id="courseDescription" maxlength=255></textarea></div>
+						<div><button id="createCourseButton"><p>Create course</p></button></div>
+						<div id="message"></div>
+        </div>
+    </body>
+</html>
---
+++ frontend/src/course-editor/js/course-editor.js
@@ -0,0 +1,58 @@
+
+(function() {
+	const courseName = document.getElementById("courseName");
+	const courseColor = document.getElementById("courseColor");
+	const courseDescription = document.getElementById("courseDescription");
+	const createCourseButton = document.getElementById("createCourseButton");
+	const message = document.getElementById("message");
+
+	function getRandomColor() {
+		const values = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+		let ret = "#";
+		for(let i = 0; i < 6; ++i) {
+			ret += values[Math.floor(Math.random() * values.length)];
+		}
+		return ret;
+	}
+	courseColor.value = getRandomColor();
+
+	function createCourse() {
+		return new Promise((resolve, reject) => {
+			DbCom.createCourse(courseName.value, courseDescription.value, courseColor.value.substring(1)).then(() => {
+				resolve();
+			}).catch((result) => {
+				reject(result);
+			});
+		});
+	}
+
+	function showMessage(text, isError) {
+		message.textContent = text;
+		if(isError) {
+			message.className = "error";
+		} else {
+			message.className = "";
+		}
+	}
+
+	createCourseButton.addEventListener("click", () => {
+		if(courseName.reportValidity() && courseDescription.reportValidity()) {
+			createCourseButton.innerHTML = "<p>. . .</p>";
+			createCourseButton.disabled = true;
+
+			createCourse().then(() => {
+				courseName.value = "";
+				courseColor.value = getRandomColor();
+				courseDescription.value = "";
+				showMessage("", false);
+			}).catch((result) => {
+				if(result.hasOwnProperty("error")) {
+					showMessage("Error: " + result.error, true);
+				}
+			}).finally(() => {
+				createCourseButton.innerHTML = "<p>Create course</p>";
+				createCourseButton.disabled = false;
+			});
+		}
+	});
+})();
