5b85176df6945f0fd1ec525a94300a6fcc10b5d1
osksun <37369260+osksun@users.noreply.github.com>
2020-10-28 16:27:51 +0100

    Add onWindowResize

--- frontend/src/index/js/3d-visualization.js
+++ frontend/src/index/js/3d-visualization.js
@@ -37,4 +37,12 @@ function onMouseMove(event) {
 
 }
 
+function onWindowResize() {
+    let aspect = window.innerWidth / window.innerHeight;
+    camera.left   = - frustumSize * aspect / 2;
+    camera.right  =   frustumSize * aspect / 2;
+    camera.top    =   frustumSize / 2;
+    camera.bottom = - frustumSize / 2;
+    camera.updateProjectionMatrix();
+    renderer.setSize( window.innerWidth, window.innerHeight );
 }
