8c67984299ad002316a85aa1db0a7a6ebf639d77
Jonathan <>
2020-10-29 10:34:03 +0100

    Fixed indentation

--- frontend/src/question-editor/js/question-editor.js
+++ frontend/src/question-editor/js/question-editor.js
@@ -1,18 +1,21 @@
+
 window.addEventListener("load", function() {
 	MathJax.startup.document.getMathItemsWithin(document.body);
-    const contentInput = document.getElementById("latex-input");
+  const contentInput = document.getElementById("latex-input");
 	const contentLatex = document.getElementById("latex-output");
 	const submitButton = document.getElementById("submit-button");
-    contentInput.addEventListener("input", () => {
+
+  contentInput.addEventListener("input", () => {
 		contentLatex.innerText = contentInput.value;
 		MathJax.texReset(0);
 		MathJax.typesetClear();
 		MathJax.typesetPromise();
-    });
+  });
 	enableTab(contentInput);
 	enableEscape(contentInput);
-    function enableTab(textarea) {
-        textarea.addEventListener("keydown", function(e) {
+
+  function enableTab(textarea) {
+  	textarea.addEventListener("keydown", function(e) {
 			if (e.keyCode === 9) { // tab was pressed
 				// get caret position/selection
 				var start = textarea.selectionStart;
@@ -25,8 +28,9 @@ window.addEventListener("load", function() {
 				// prevent the focus lose
 				e.preventDefault();
 			}
-        });
+    });
 	}
+
 	function enableEscape(textarea) {
 		textarea.addEventListener("keydown", function(e) {
 			if (e.keyCode === 27) { // escape was pressed
