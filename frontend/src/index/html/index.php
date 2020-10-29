00b9545faf1571965a5748997f516b44c1848bc1
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-10-29 12:15:02 +0100

    Move scripts to related line

--- frontend/src/index/html/index.php
+++ frontend/src/index/html/index.php
@@ -4,20 +4,17 @@
 		<title>All courses</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" type="text/css" href="/src/shared/css/fixed-content.css">
 		<link rel="stylesheet" type="text/css" href="/src/shared/css/body.css">
 		<link rel="stylesheet" type="text/css" href="/src/index/css/index.css">
 		<link rel="stylesheet" type="text/css" href="/src/index/css/courses-view.css">
 		<link rel="stylesheet" type="text/css" href="/src/index/css/modules-view.css">
 		<link rel="stylesheet" type="text/css" href="/src/index/css/question-view.css">
-		<link rel="stylesheet" type="text/css" href="/src/shared/css/fixed-content.css">
 		<script src="/src/shared/js/mathjax-config.js" defer></script>
-		<script type="text/javascript" id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js" defer></script>
-		<script type="application/javascript" src="/src/shared/js/backend-communication.js"></script>
-		<script type="application/javascript" src="/src/shared/js/fixed-content.js" defer></script>
-		<script type="application/javascript" src="/src/index/js/index.js"></script>
-		<script type="application/javascript" src="/src/index/js/courses-view.js" defer></script>
-		<script type="application/javascript" src="/src/index/js/modules-view.js" defer></script>
-		<script type="application/javascript" src="/src/index/js/question-view.js" defer></script>
+		<script id="MathJax-script" src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js" defer></script>
+		<script src="/src/shared/js/backend-communication.js"></script>
+		<script src="/src/shared/js/fixed-content.js" defer></script>
+		<script src="/src/index/js/index.js"></script>
 	</head>
 	<body class="tex2jax_ignore">
 		<?php include "../../shared/html/fixed-content.html"; ?>
@@ -30,10 +27,13 @@
 				</div>
 				<div id="view-container" class="view-container">
 					<ul id="courses-view">
+						<script src="/src/index/js/courses-view.js" defer></script>
 					</ul>
 					<ul id="modules-view">
+						<script src="/src/index/js/modules-view.js" defer></script>
 					</ul>
 					<ul id="question-view">
+						<script src="/src/index/js/question-view.js" defer></script>
 						<?php include "./question-view.html"; ?>
 					</ul>
 				</div>
