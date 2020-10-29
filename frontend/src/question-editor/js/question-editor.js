a16b7d22bd7825a5c50a6d113ce22d6ec1ae3193
Jonathan <>
2020-10-29 11:53:37 +0100

    Question editor answer regex

--- frontend/src/question-editor/js/question-editor.js
+++ frontend/src/question-editor/js/question-editor.js
@@ -3,13 +3,19 @@ window.addEventListener("load", function() {
 	MathJax.startup.document.getMathItemsWithin(document.body);
   const contentInput = document.getElementById("latex-input");
 	const contentLatex = document.getElementById("latex-output");
-	const submitButton = document.getElementById("submit-button");
+	const answerRegexText = document.getElementById("question-answer-regex");
+	const submitButton = document.getElementById("create-question-button");
+	const message = document.getElementById("question-editor-message");
 
-  contentInput.addEventListener("input", () => {
+	function refreshLatex() {
 		contentLatex.innerText = contentInput.value;
 		MathJax.texReset(0);
 		MathJax.typesetClear();
 		MathJax.typesetPromise();
+	}
+
+  contentInput.addEventListener("input", () => {
+		refreshLatex();
   });
 	enableTab(contentInput);
 	enableEscape(contentInput);
@@ -38,4 +44,34 @@ window.addEventListener("load", function() {
 			}
 		});
 	}
+
+	function showMessage(text, isError) {
+		message.textContent = text;
+		if(isError) {
+			message.className = "error";
+		} else {
+			message.className = "";
+		}
+	}
+
+	submitButton.addEventListener("click", (event) => {
+		submitButton.innerHTML = "<p>. . .</p>";
+		submitButton.disabled = true;
+
+		const moduleID = 1;//TODO use correct moduleID
+		const content = contentInput.value;
+		const answer = answerRegexText.value;
+		DbCom.createQuestion(moduleID, content, answer).then(() => {
+			contentInput.value = "";
+			answerRegexText.value = "";
+			refreshLatex();
+		}).catch((result) => {
+			if(result.hasOwnProperty("error")) {
+				showMessage("Error: " + result.error, true);
+			}
+		}).finally(() => {
+			submitButton.innerHTML = "<p>Create question</p>";
+			submitButton.disabled = false;
+		});
+	});
 });
