8dc0ec632ce55a714325c5b16b77b2cfac90ccf1
osksun <37369260+osksun@users.noreply.github.com>
2020-11-09 12:56:31 +0100

    Rename fixed-content to top-bar

--- frontend/src/settings/settings.php
+++ frontend/src/settings/settings.php
@@ -6,13 +6,13 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="/src/shared/css/body.css">
 		<link rel="stylesheet" type="text/css" href="/src/settings/settings.css">
-		<link rel="stylesheet" type="text/css" href="/src/shared/css/fixed-content.css">
+		<link rel="stylesheet" type="text/css" href="/src/shared/css/top-bar.css">
 		<script src="/src/shared/js/backend-communication.js"></script>
-		<script src="/src/shared/js/fixed-content.js" defer></script>
+		<script src="/src/shared/js/top-bar.js" defer></script>
 		<script src="/src/settings/settings.js" defer></script>
 	</head>
 	<body>
-		<?php include "../shared/html/fixed-content.html"; ?>
+		<?php include "../shared/html/top-bar.html"; ?>
 		<div id="page-content">
 			<div><h3>Current password</h3><input id="current-password" type="password" autocomplete="current-password" required/></div>
 			<div><h3>New password</h3><input id="new-password" type="password" autocomplete="new-password" required/></div>
