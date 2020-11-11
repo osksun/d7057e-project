37004f1966d5b9ad50bcc6ee8d3a5de815d818cb
Jonathan <25746136+ojaebe-6@users.noreply.github.com>
2020-11-11 15:26:51 +0100

    Nicer settings page

--- frontend/src/settings/settings.php
+++ frontend/src/settings/settings.php
@@ -7,6 +7,7 @@
 		<link rel="stylesheet" type="text/css" href="/src/shared/css/body.css">
 		<link rel="stylesheet" type="text/css" href="/src/settings/settings.css">
 		<link rel="stylesheet" type="text/css" href="/src/shared/css/top-bar.css">
+		<link rel="stylesheet" type="text/css" href="/src/shared/css/theme.css">
 		<script src="/src/shared/js/backend-communication.js"></script>
 		<script src="/src/shared/js/top-bar.js" defer></script>
 		<script src="/src/settings/settings.js" defer></script>
@@ -14,11 +15,12 @@
 	<body>
 		<?php include "../shared/html/background.html"; ?>
 		<?php include "../shared/html/top-bar.html"; ?>
+		<h2>Change password</h2>
 		<div id="page-content">
 			<div><h3>Current password</h3><input id="current-password" type="password" autocomplete="current-password" required/></div>
 			<div><h3>New password</h3><input id="new-password" type="password" autocomplete="new-password" required/></div>
-			<div><h3>Retype new password</h3><input id="new-password-confirm" type="password" autocomplete="new-password" required/></div>
-			<div><button id="change-password-button"><p>Change password</p></button></div>
+			<div><h3>Confirm new password</h3><input id="new-password-confirm" type="password" autocomplete="new-password" required/></div>
+			<div><button id="change-password-button" class="button">Change password</button></div>
 			<div id="message"></div>
 		</div>
 	</body>
