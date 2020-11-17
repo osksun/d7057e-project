7ae63f762f401b32023906ffccb03e15d46a88a4
Amadeus Johansson <maxibrillion@gmail.com>
2020-11-17 13:51:42 +0100

    Database course deletion

--- backend/main/database.js
+++ backend/main/database.js
@@ -16,11 +16,11 @@ function connect() {
 
 				const tables = [
 					"CREATE TABLE IF NOT EXISTS userdata (id INT NOT NULL, xp BIGINT DEFAULT 0, isAdmin BOOL DEFAULT false, PRIMARY KEY(id))",
-					"CREATE TABLE IF NOT EXISTS courses (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(255) NOT NULL, color CHAR(6) NOT NULL, PRIMARY KEY(id))",
-					"CREATE TABLE IF NOT EXISTS modules (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, courseID int NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(courseID) REFERENCES courses(id), CONSTRAINT uniqueModuleCourse UNIQUE (name, courseID))",
-					"CREATE TABLE IF NOT EXISTS questions (id INT NOT NULL AUTO_INCREMENT, moduleID INT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(moduleID) REFERENCES modules(id))",
-					"CREATE TABLE IF NOT EXISTS questionsegments (questionID INT NOT NULL, segmentOrder INT NOT NULL, type VARCHAR(255) NOT NULL, content TEXT NOT NULL, answer TEXT, PRIMARY KEY(questionID, segmentOrder), FOREIGN KEY(questionID) REFERENCES questions(id))",
-					"CREATE TABLE IF NOT EXISTS answers (userID int NOT NULL, questionID int NOT NULL, PRIMARY KEY(userID, questionID), FOREIGN KEY(userID) REFERENCES userdata(id), FOREIGN KEY(questionID) REFERENCES questions(id))",
+					"CREATE TABLE IF NOT EXISTS courses (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) UNIQUE NOT NULL, description VARCHAR(255) NOT NULL, color CHAR(6) NOT NULL, PRIMARY KEY(id), deleteon DATE)",
+					"CREATE TABLE IF NOT EXISTS modules (id INT NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL, courseID int NOT NULL, description VARCHAR(255) NOT NULL, PRIMARY KEY(id), FOREIGN KEY(courseID) REFERENCES courses(id) ON DELETE CASCADE, CONSTRAINT uniqueModuleCourse UNIQUE (name, courseID))",
+					"CREATE TABLE IF NOT EXISTS questions (id INT NOT NULL AUTO_INCREMENT, moduleID INT NOT NULL, PRIMARY KEY(id), FOREIGN KEY(moduleID) REFERENCES modules(id) ON DELETE CASCADE)",
+					"CREATE TABLE IF NOT EXISTS questionsegments (questionID INT NOT NULL, segmentOrder INT NOT NULL, type VARCHAR(255) NOT NULL, content TEXT NOT NULL, answer TEXT, PRIMARY KEY(questionID, segmentOrder), FOREIGN KEY(questionID) REFERENCES questions(id) ON DELETE CASCADE)",
+					"CREATE TABLE IF NOT EXISTS answers (userID int NOT NULL, questionID int NOT NULL, PRIMARY KEY(userID, questionID), FOREIGN KEY(userID) REFERENCES userdata(id), FOREIGN KEY(questionID) REFERENCES questions(id) ON DELETE CASCADE)",
 					"CREATE TABLE IF NOT EXISTS moderators (userID INT NOT NULL, courseID INT NOT NULL, PRIMARY KEY(userID, courseID), FOREIGN KEY(userID) REFERENCES userdata(id), FOREIGN KEY(courseID) REFERENCES courses(id))"
 				];
 
@@ -144,9 +144,36 @@ function updateCourse(courseID, name, description, color) {
 }
 exports.updateCourse = updateCourse;
 
+function softDeleteCourse(courseID) {
+	return new Promise((resolve, reject) => {
+		connection.query("UPDATE courses SET deleteon = DATE_ADD(CURRENT_DATE, INTERVAL 30 DAY) WHERE id = ?", [courseID], (error, result) => {
+			if(error) {
+				reject();
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+exports.softDeleteCourse = softDeleteCourse;
+
+function purgeExpiredCourses() {
+	return new Promise((resolve, reject) => {
+		connection.query("DELETE FROM courses WHERE deleteon <= CURRENT_DATE", (error, result) => {
+			if(error) {
+				console.log(error);
+				reject();
+			} else {
+				resolve();
+			}
+		});
+	});
+}
+exports.purgeExpiredCourses = purgeExpiredCourses;
+
 function getCourseByName(name) {
 	return new Promise((resolve, reject) => {
-		connection.query("SELECT id, name, description, color FROM courses WHERE name=?", [name], (error, result) => {
+		connection.query("SELECT id, name, description, color FROM courses WHERE name=? AND deleteon IS NULL", [name], (error, result) => {
 			if(error) {
 				reject();
 			} else {
@@ -169,6 +196,7 @@ function getCourses(userID) {
 			RIGHT JOIN questions ON questions.id = answers.questionID AND userID=?
 			INNER JOIN modules ON modules.id = questions.moduleID
 			RIGHT JOIN courses ON courses.id = modules.courseID
+			WHERE courses.deleteon IS NULL
 			GROUP BY courses.id
 			`, [userID], (error, result) => {
 			if(error) {
