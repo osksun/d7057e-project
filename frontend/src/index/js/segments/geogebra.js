381c871a3a14d511487f1db1dcbabc19dcd2d2d0
osksun <37369260+osksun@users.noreply.github.com>
2020-11-17 12:28:51 +0100

    Add GeoGebra segments

--- frontend/src/index/js/segments/geogebra.js
+++ frontend/src/index/js/segments/geogebra.js
@@ -1,13 +1,13 @@
 (function(){
 	let idIncrement = 0;
-	questionEditor.addSegmentType("GEOGEBRA", "GeoGebra", function(content = "") {
+	questionViewManager.addSegmentType("GEOGEBRA", function(content) {
 		const superDiv = document.createElement("div");
 		const div = document.createElement("div");
 		const defaultStateButton = document.createElement("button");
 		defaultStateButton.onclick = function() {loadState()};
 		superDiv.appendChild(div);
 		superDiv.appendChild(defaultStateButton);
-		div.id = "ggb-editElement" + idIncrement;
+		div.id = "ggb-viewElement" + idIncrement;
 		let ggbApp;
 		let ggbID;
 		setTimeout(createGGBAPP, 0);
@@ -24,28 +24,17 @@
 				"useBrowserForJS":false,
 				"ggbBase64": content
 			}, "5.0");
-			ggbApp.inject('ggb-editElement' + idIncrement);
+			ggbApp.inject('ggb-viewElement' + idIncrement);
 			idIncrement++;
-		}
-		
-		function saveGGBState(){
-			const saveState = window[ggbID].getBase64();
-			return saveState;
 		}
 
 		function loadState(){
 			const newState = window[ggbID].setBase64(content);
 			ggbApp.ggbBase64 = newState;
 		}
-
 		return {
 			div:superDiv,
-			getContent:() => {
-				return saveGGBState();
-			},
-			getAnswer:() => {
-				return null;
-			}
+			input:null
 		};
 	});
 })();
