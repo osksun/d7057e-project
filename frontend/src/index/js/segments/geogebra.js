aed536d4e474a2eca9d918b4a8e4d8d65813d7a2
Markus Jonsson <30708674+MarqusJonsson@users.noreply.github.com>
2020-11-20 17:52:46 +0100

    Fix GeoGebra question segment

--- frontend/src/index/js/segments/geogebra.js
+++ frontend/src/index/js/segments/geogebra.js
@@ -1,30 +1,40 @@
 (function(){
 	let idIncrement = 0;
 	questionViewManager.addSegmentType("GEOGEBRA", function(content) {
-		const superDiv = document.createElement("div");
+		const container = document.createElement("div");
+		container.className = "geogebra-container";
+		const scalingContainer = document.createElement("div");
+		scalingContainer.className = "geogebra-scaling-container";
 		const div = document.createElement("div");
-		let ggbApp;
-		let ggbID;
-		setTimeout(createGGBAPP, 0);
+		div.className = "geogebra-div";
+		scalingContainer.appendChild(div);
+		container.appendChild(scalingContainer);
+
 		const defaultStateButton = document.createElement("button");
 		defaultStateButton.innerText = "Revert to default";
 		defaultStateButton.classList.add("button");
 		defaultStateButton.onclick = function() {loadGGBState();};
-		superDiv.appendChild(div);
-		superDiv.appendChild(defaultStateButton);
+		container.appendChild(defaultStateButton);
+
+		let ggbApp;
+		let ggbID;
+		setTimeout(createGGBAPP, 0);
+
 		function createGGBAPP() {
 			div.id = "ggb-element" + idIncrement;
 			ggbID = "geogebraApp" + idIncrement;
 			ggbApp = new GGBApplet({
 				"id": ggbID,
-				"appName": "3d",
-				"width": 800,
-				"height": 600,
+				"appName": "classic",
+				"width": 1280,
+				"height": 720,
+				"ggbBase64": content,
 				"showToolBar": true,
-				"showAlgebraInput": true,
 				"showMenuBar": true,
-				"useBrowserForJS": false,
-				"ggbBase64": content
+				"showResetIcon": true,
+				"useBrowserForJS": true,
+				"allowUpscale": true,
+				"scaleContainerClass": "geogebra-scaling-container"
 			}, "5.0");
 			ggbApp.inject('ggb-element' + idIncrement);
 			idIncrement++;
@@ -34,7 +44,7 @@
 			ggbApp.ggbBase64 = window[ggbID].setBase64(content);;
 		}
 		return {
-			div:superDiv,
+			div:container,
 			input:null
 		};
 	});
