aa1699b64d4a511b8c4469d4334ddf1d5ac1a01f
osksun <37369260+osksun@users.noreply.github.com>
2020-11-27 14:24:58 +0100

    Add explicit render of RECaptcha

--- frontend/src/register/register.php
+++ frontend/src/register/register.php
@@ -10,26 +10,40 @@
 		<link rel="stylesheet" type="text/css" href="/src/shared/css/theme.css">
 		<link rel="stylesheet" type="text/css" href="/src/register/error-box.css">
 		<script src="/src/shared/js/backend-communication.js"></script>
-		<script src= "https://www.google.com/recaptcha/api.js" async defer></script>
+		<script src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit" async defer> </script>
+
 	</head>
+	<script type="text/javascript">
+		var onloadCallback = function() {
+			console.log("grecaptcha is ready!", );
+			grecaptcha.render('reg-captcha', {
+				'sitekey': "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" ,
+				'theme': "dark",
+				'callback': 'userResponseCallback'
+			})
+		};
+		var userResponseCallback = function () {
+			captchaVerification(grecaptcha.getResponse());
+		}
+	</script>
 
 	<body>
 		<?php include "../shared/html/background.html";?>
-
 		<h2>REGISTER</h2>
-		<div id="container">
+		<div id="container"> 
 			<label>Email</label>
 			<input id="emailField" type="text" required placeholder="email@example.com">
-
+			
 			<label>Password</label>
 			<input id="passwordField" type="password" required placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
-
+			
 			<label>Confirm Password</label>
 			<input id="repeatPasswordField" type="password" required placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;">
-
+			
 			<button id="registerButton" class="button">Register</button>
+			<div id = "reg-captcha" class="g-recaptcha"> </div>
 		</div>
-		<div id = "captcha" class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI" data-theme="dark"> </div>
+		
 		<div id="error-box"></div>
 		<a href="/login" class="button">Login</a>
 
